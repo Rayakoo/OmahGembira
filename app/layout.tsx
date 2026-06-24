5f7d4e5f49064c5d2914fe6b2718e1407731e8a2
Rayakoo <rafianantan@gmail.com>
2026-06-24 14:17:07 +0700

    aksesibilitas button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import AccessibilityWidget from "@/components/AccessibilityWidget";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
+        <AccessibilityWidget />
       </body>
     </html>
   );
